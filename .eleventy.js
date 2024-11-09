4eb0e2e0ee50f58973e02e9796c39e503c211926
Hemant Shashikant Yadav. <hemantsryadav@gmail.com>
2024-11-09 09:19:44 +0530

    routing fixed, layout added for Accessories & Components page and image path fixed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/services");
   return {
     dir: {
       input: "src",
       output: "dist",
+      includes: "_includes",
     },
+    
   };
 };
